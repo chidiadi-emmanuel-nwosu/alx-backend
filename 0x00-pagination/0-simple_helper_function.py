cf8433112b0b249eb164689b932d07e8c3097507
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-27 09:00:48 +0100

    debugging 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ 0-simple_helper_function """
-from typing import Union
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple:
     """
     Calculate the start and end indexes for pagination.
 
