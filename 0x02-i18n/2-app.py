578c60bae8c6636f7ef34416dfc6c3d68e531ec4
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-01-09 14:59:11 +0100

    fixed

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,7 +20,7 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale() -> str | None:
+def get_locale() -> str:
     """ get_locale
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
